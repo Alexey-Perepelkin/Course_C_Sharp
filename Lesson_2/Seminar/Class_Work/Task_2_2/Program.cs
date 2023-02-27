bfbcbed9315aa6648d495e246cc2256348b22aa5
Alexey Perepelkin <kip_a@list.ru>
2023-02-27 16:21:04 +0500

    Add Task_2_2

---
+++ Lesson_2/Seminar/Class_Work/Task_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// 3. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деление.
+void Task3Num(int num1, int num2)
+{
+    Console.WriteLine( num1 );
+    Console.WriteLine( num2 );
+    if (num1 % num2 == 0) Console.WriteLine("Кратно");
+    else Console.WriteLine($"не кратно,остаток   { num1 % num2}"); 
+}
+Task3Num(34,6);
