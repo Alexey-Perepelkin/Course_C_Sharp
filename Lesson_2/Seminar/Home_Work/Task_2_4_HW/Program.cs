600a811968e97bc3c040d7d96c98ceee9fe7127c
Alexey Perepelkin <kip_a@list.ru>
2023-03-01 00:23:21 +0500

    Task_2_4_HW version 1.1

--- Lesson_2/Seminar/Home_Work/Task_2_4_HW/Program.cs
+++ Lesson_2/Seminar/Home_Work/Task_2_4_HW/Program.cs
@@ -5,34 +5,39 @@
 int CountDays (int year,int month,int day)
 {
     //int[,] Months = new int[12];
-    int[] CountDaysMonth = {31,29,31,30,31,30,31,31,30,31,30,31};
-    if (year % 4 == 0) CountDaysMonth [1]=28;
+    int[] CountDaysMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if (year % 4 == 0) CountDaysMonth [1]=29;
     int index = 0;
     int tmpday = 0 ;
     int tmpmonth = 0;
-    while (index < month)
-    {
-        tmpday= day + tmpmonth; 
-        tmpmonth= tmpmonth + CountDaysMonth[index];
-
-    }
+    if (day > CountDaysMonth[month-1]) tmpday = -1;
+    else
+        while (index < month)
+        {
+            tmpday= day + tmpmonth; 
+            tmpmonth= tmpmonth + CountDaysMonth[index];
+            index ++;
+        }
     return tmpday;
 }
-int [,] StartYears ()
+int  StartYears (int year)
 {
-    int[] FirstDayYears ={0,1,2,3,4,6,0,1,2,4,5,6,0,2,3,4,5,0,1,2,3,5,6,0,1,3,4,5};
-    int[ , ] Years = new int[28,2];
+    int[] FirstDayYears ={7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6};
+    int[  ] Years = new int[28];
     int index = 0;
-    int Year = 1900;
-    // Console.WriteLine (FirstDayYears.Length);
+    int YearTemp = 1900;
+    int firstDay = -1;
     while (index < FirstDayYears.Length)
     {
-        Years[index,0] = Year;
-        Years[index,1] = FirstDayYears[index];
-        Year++;
+        if ((year-YearTemp) % 28 == 0) 
+        {
+           firstDay = FirstDayYears[index];
+        }    
+        YearTemp++;
         index++;
     }
-    return Years ;
+
+    return firstDay ;
 }
 // string[] DaysWeek {"ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"};
 // int[] Months {01,02,03,04,05,06,07,08,09,10,11,12};
@@ -43,5 +48,5 @@ int [,] StartYears ()
 // }
 
 //int [ , ] t = StartYears();
- Console.WriteLine ( CountDays (2020, 01, 22));
+ Console.WriteLine ( StartYears (2012));
 // Console.WriteLine (t[0,1]);
