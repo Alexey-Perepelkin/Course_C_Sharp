7b216159b4899154eb2dfe2ddf375bd13c6ec2cf
Alexey Perepelkin <kip_a@list.ru>
2023-02-28 15:13:22 +0500

    Task_2_3 version 1.0 Not Function

--- Lesson_2/Seminar/Home_Work/Task_2_3_HW/Program.cs
+++ Lesson_2/Seminar/Home_Work/Task_2_3_HW/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Задача 3: Напишите программу, которая выводит третью
 // цифру заданного числа или сообщает, что третьей цифры
 // нет.
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
 
-int a! = Concole.ReadLine()!;
+if (a<100) Console.WriteLine("Число не имеет третий разряд");
+else
+    {  
+    while ( a>1000 )
+        {
+            a = a / 10;
+            Console.WriteLine("---------");
+            Console.WriteLine(a);
+            
+        }
+    Console.WriteLine("** Результат **");    
+    Console.WriteLine(a % 10);
+    }
