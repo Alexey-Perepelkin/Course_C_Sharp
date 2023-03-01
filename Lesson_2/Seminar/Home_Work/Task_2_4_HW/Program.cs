b3b338f69af1e66a5324d8f948cbde984e8dd21b
Alexey Perepelkin <kip_a@list.ru>
2023-03-01 11:29:35 +0500

    Task_2_4_HW version 1.2

--- Lesson_2/Seminar/Home_Work/Task_2_4_HW/Program.cs
+++ Lesson_2/Seminar/Home_Work/Task_2_4_HW/Program.cs
@@ -4,7 +4,6 @@
 
 int CountDays (int year,int month,int day)
 {
-    //int[,] Months = new int[12];
     int[] CountDaysMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
     if (year % 4 == 0) CountDaysMonth [1]=29;
     int index = 0;
@@ -20,10 +19,9 @@ int CountDays (int year,int month,int day)
         }
     return tmpday;
 }
-int  StartYears (int year)
+int  FirstDayYear (int year)
 {
     int[] FirstDayYears ={7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6};
-    int[  ] Years = new int[28];
     int index = 0;
     int YearTemp = 1900;
     int firstDay = -1;
@@ -39,14 +37,34 @@ int  StartYears (int year)
 
     return firstDay ;
 }
-// string[] DaysWeek {"ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"};
-// int[] Months {01,02,03,04,05,06,07,08,09,10,11,12};
-// int StartYears[] = new int[28]
-// while (true)
-// {
-    
-// }
 
-//int [ , ] t = StartYears();
- Console.WriteLine ( StartYears (2012));
-// Console.WriteLine (t[0,1]);
+
+int Sum (int a, int d)
+{
+    int summa = a+d-1;
+    int result = 0;
+    if (summa <8) result = summa;
+    else result =summa % 7;
+    return result;
+}
+
+
+
+Console.WriteLine("Введите число (DD)");
+int InDay = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите месяц (MM)");
+int InMonth = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите год (YYYY)");
+int InYear = int.Parse(Console.ReadLine()!);
+string[] DaysWeek = {"ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"};
+
+if (CountDays(InYear,InMonth,InDay) == -1) Console.WriteLine("Такого числа нет в этом месяце");
+else
+{
+    int Day = FirstDayYear(InYear);
+    int DayEnd = CountDays(InYear,InMonth,InDay) % 7;
+    int index = Sum (Day,DayEnd)-1;
+    Console.WriteLine (DaysWeek[index]);
+    if (index == 5 | index == 6) Console.WriteLine("Выходной день");
+    else Console.WriteLine("Рабочий день");
+}    
