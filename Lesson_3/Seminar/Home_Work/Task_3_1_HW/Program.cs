438b6ede5e748de2ff231c9a5fad1cc53a284e1a
Alexey Perepelkin <kip_a@list.ru>
2023-03-06 15:38:10 +0500

    Add Task 3_1 HW

---
+++ Lesson_3/Seminar/Home_Work/Task_3_1_HW/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+
+
+int NumberDigit(int num)
+{
+    int count = 1;
+    while (num > 10)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+void NumberCheck(int num, int digits)
+
+{
+    int count = 0;
+    for (int i = 0; i < digits / 2; i++)
+    {
+        int FirstDigitNum = num % 10;
+        int LastDigitNum = num / Convert.ToInt32(Math.Pow(10, (digits - 1) - count));
+        if (FirstDigitNum == LastDigitNum)
+        {
+            num = num / 10;
+            num = num % Convert.ToInt32(Math.Pow(10, (digits - 2) - count));
+            count = count + 2;
+        }
+        else
+        {
+            Console.WriteLine("Это не палиндром");
+            return;
+        }
+    }
+    Console.WriteLine("Это палиндром");
+}
+
+Console.WriteLine("Введите число");
+int Number = int.Parse(Console.ReadLine()!);
+int DigitCount = NumberDigit(Number);
+NumberCheck(digits: DigitCount, num: Number);
+
