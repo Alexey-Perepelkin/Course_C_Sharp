075f981477be730074d332c6b4d30f97c28f2d75
Alexey Perepelkin <kip_a@list.ru>
2023-03-07 11:53:15 +0500

    Add Task 4_3_HW

---
+++ Lesson_4/Seminar/Home_Work/Task_4_3_HW/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+
+int[] Numbers(int arrLen)
+{
+    int[] arr = new int[arrLen];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("}");
+}
+
+Console.Write("Введите размерность массива: ");
+int LenArr = int.Parse(Console.ReadLine()!);
+
+int[] Array = Numbers(LenArr);
+PrintArr(Array);
+
+Console.WriteLine();
