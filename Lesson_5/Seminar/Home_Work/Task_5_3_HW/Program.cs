00b1f9e8a1d773601defcb828feca276d1a605a6
Perepelkin Alexey <kip_a@list.ru>
2023-03-12 16:46:48 +0300

    Add Task 5_3_HW

--- Lesson_5/Seminar/Home_Work/Task_5_3_HW/Program.cs
+++ Lesson_5/Seminar/Home_Work/Task_5_3_HW/Program.cs
@@ -1,43 +1,45 @@
-﻿
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
 
-void Print(int[] arr)
+void Print(double[] arr)
 {
     int size = arr.Length;
-
+    Console.Write($"Array: ");
     for (int i = 0; i < size; i++)
-        Console.Write($"Array: {arr[i]} ");
+        Console.Write($"{arr[i]} ");
     Console.WriteLine();
 }
 
-int[] MassNums(int size, int from, int to)
+double[] MassNums(int size, int from, int to)
 {
-    int[] arr = new int[size];
+    double[] arr = new double[size];
 
     for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(from, to);
+        arr[i] = Math.Round( new Random().NextDouble() * (to-from)+from,2);
     return arr;
 }
 
-void SumPosNeg(int[] arr)
+void SumPosNeg(double[] arr)
 {
-    int pos, neg;
-    pos = neg = 0;
+    double min = arr[0];
+    double max = arr[0];
+    string result = string.Empty;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > 0)
-            pos += arr[i];
-        else
-            neg += arr[i];
+        if (arr[i] > max) max = arr[i];
+        else if (arr[i] < min) min = arr[i];
     }
-    Console.WriteLine($"Positive: {pos}, negative: {neg}");
+    result = $"min number = {min} ---- max number = {max}";
+    Console.WriteLine(result);
 }
-Console.WriteLine("Input size massive:");
+Console.WriteLine("Input size array: ");
 int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Input the lover bound array:");
+Console.WriteLine("Input the lover bound random: ");
 int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Input the upper bound array:");
+Console.WriteLine("Input the upper bound random: ");
 int stop = int.Parse(Console.ReadLine()!);
 
-int[] mass = MassNums(num, start, stop);
+double[] mass = MassNums(num, start, stop);
 Print(mass);
 SumPosNeg(mass);
