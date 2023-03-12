d462b92877e02848b26bce61c68a8d069f0a1369
Perepelkin Alexey <kip_a@list.ru>
2023-03-12 17:39:31 +0300

    Add Task 5_4_HW

--- Lesson_5/Seminar/Home_Work/Task_5_4_HW/Program.cs
+++ Lesson_5/Seminar/Home_Work/Task_5_4_HW/Program.cs
@@ -1,11 +1,13 @@
-﻿
+﻿// Задача 4: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
 
 void Print(int[] arr)
 {
     int size = arr.Length;
-
+    Console.Write($"Array: ");
     for (int i = 0; i < size; i++)
-        Console.Write($"Array: {arr[i]} ");
+        Console.Write($"{arr[i]} ");
     Console.WriteLine();
 }
 
@@ -18,26 +20,30 @@ int[] MassNums(int size, int from, int to)
     return arr;
 }
 
-void SumPosNeg(int[] arr)
+int [] NewArr(int[] arr)
 {
-    int pos, neg;
-    pos = neg = 0;
-    for (int i = 0; i < arr.Length; i++)
+    int size = arr.Length;
+    int newSize = size / 2 + size % 2;
+    int[] newArr = new int[newSize];
+
+    for (int i = 0; i < size / 2; i++)
     {
-        if (arr[i] > 0)
-            pos += arr[i];
-        else
-            neg += arr[i];
+        newArr[i] = arr[i] * arr[size - 1 - i];
     }
-    Console.WriteLine($"Positive: {pos}, negative: {neg}");
+    if (size%2==1) newArr[newSize-1]=arr[newSize-1];
+    return newArr; 
+    
 }
-Console.WriteLine("Input size massive: ");
+
+Console.WriteLine("Input size array: ");
 int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Input the lover bound array: ");
+Console.WriteLine("Input the lover bound random: ");
 int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Input the upper bound array: ");
+Console.WriteLine("Input the upper bound random: ");
 int stop = int.Parse(Console.ReadLine()!);
 
 int[] mass = MassNums(num, start, stop);
 Print(mass);
-SumPosNeg(mass);
+NewArr(mass);
+// Console.WriteLine(NewArr(mass));
+Print(NewArr(mass));
