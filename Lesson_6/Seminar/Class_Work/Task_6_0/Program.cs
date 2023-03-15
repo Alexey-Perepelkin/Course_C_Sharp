150c54345ee11977c276cef5d86f0eac6d4c7fc8
Perepelkin Alexey <kip_a@list.ru>
2023-03-15 12:18:10 +0300

    hw

---
+++ Lesson_6/Seminar/Class_Work/Task_6_0/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 1: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    Console.Write($"Array: ");
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void RewMas(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        (arr[i], arr[size - i - 1]) = (arr[size - i - 1], arr[i]);
+    }
+}
+
+int num;
+int start;
+int stop;
+
+Console.WriteLine("Input size array: ");
+string InSize = Console.ReadLine()!;
+bool resiltInSize = int.TryParse(InSize, out num);
+
+if (!resiltInSize)
+{
+    Console.WriteLine("Not correct size array!");
+    return;
+}
+
+Console.WriteLine("Input the lover bound random: ");
+string LBoundRand = Console.ReadLine()!;
+bool resultInLBound = int.TryParse(LBoundRand, out start);
+
+if (!resultInLBound)
+{
+    Console.WriteLine("Not correct lover bound random!");
+    return;
+}
+
+Console.WriteLine("Input the upper bound random: ");
+string UBoundRand = Console.ReadLine()!;
+bool resultInUBound = int.TryParse(UBoundRand, out stop);
+
+if (!resultInUBound)
+{
+    Console.WriteLine("Not correct upper bound random!");
+    return;
+}
+
+if (start > stop)
+{
+    Console.WriteLine("WARNING! Lover bound random more than" +
+    " upper bound random. Values will be changed.");
+    (start, stop) = (stop, start);
+}
+int[] mass = MassNums(num, start, stop);
+
+Print(mass);
+RewMas(mass);
+Print(mass);
---
+++ Lesson_6/Seminar/Home_Work/Task_6_2_HW/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void PointEntersection(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают");
+    
+    else
+    {
+        double crossX = (b2 - b1) / (k1 - k2);
+        double crossY = (k1 * b2 - b1) / (k1 - k2) + b1;
+        Console.WriteLine($"Точка Х = {crossX}");
+        Console.WriteLine($"Точка y = {crossY}");
+    }
+}
+
+ Console.WriteLine("Input b1: ");
+ double b1=Convert.ToDouble(Console.ReadLine()!);
+ Console.WriteLine("Input k1: ");
+ double k1=Convert.ToDouble(Console.ReadLine()!);
+ Console.WriteLine("Input b2: ");
+ double b2=Convert.ToDouble(Console.ReadLine()!);
+ Console.WriteLine("Input k2: ");
+ double k2=Convert.ToDouble(Console.ReadLine()!);
+
+ PointEntersection(b1,k1,b2,k2);
