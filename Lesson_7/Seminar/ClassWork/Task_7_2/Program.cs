705550a4bf06df610b8b4cd7fabc56cbd8bb8c85
Perepelkin Alexey <kip_a@list.ru>
2023-03-21 17:55:54 +0300

    Add NW 7

---
+++ Lesson_7/Seminar/ClassWork/Task_7_2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 2: Задайте двумерный массив. Найдите элементы, у которых
+// обе позиции чётные, и замените эти элементы на их квадраты.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+int[,] NewMass(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                arr[i, j] = arr[i, j] * arr[i, j];
+            }
+
+    }
+    return arr;
+}
+
+
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+Print(NewMass(mass));
---
+++ Lesson_7/Seminar/HomeWork/Task_7_1_HW/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+void Print(double[,] arr, int roundNum,int numDigt)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    string  convertNumDoublOnText = string.Empty;
+    string countSpace = string.Empty;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            convertNumDoublOnText = Convert.ToString(arr[i, j]);
+
+            countSpace = countSpace.PadLeft((numDigt + roundNum) -  convertNumDoublOnText.Length);
+            Console.Write($"{arr[i, j]}  {countSpace}");
+            countSpace = string.Empty;
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to, int roundNum)
+{
+    double[,] arr = new double[row, column];
+    Random newNum = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+
+            arr[i, j] = Math.Round((newNum.NextDouble() * (to - from) + from), roundNum);
+    }
+    return arr;
+}
+
+
+int countNumDigit(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count+=1;
+}
+
+Console.Write("Колличество строк в массиве = ");
+int num_row = int.Parse(Console.ReadLine()!);
+Console.Write("Колличество столбцов в массиве = ");
+int num_column = int.Parse(Console.ReadLine()!);
+Console.Write("Нижняя граница Random = ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Верхняя  граница Random = ");
+int stop = int.Parse(Console.ReadLine()!);
+Console.Write("Округлить до = ");
+int roundNum = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(num_row, num_column, start, stop, roundNum);
+Print(mass, roundNum,countNumDigit(stop));
+
